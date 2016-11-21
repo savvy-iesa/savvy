cf082d25bd887ec54bfcf394b798a7ea5a16cc61
edouardthieffry <e.thieffry62930@gmail.com>
2016-11-21 23:55:15 +0100

    gulpfile livereload & inject css in browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
-var gulp = require ('gulp');
+'use strict';
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
+gulp.task('serve', ['sass'], function() {
 
-gulp.task('hello', function() {
-    console.log('gulpgile is working');
+    browserSync.init({
+        server: "./",
+        port: 15000
+    });
+
+    gulp.watch("savvy/scss/*.scss", ['sass']);
+    gulp.watch("savvy/*.html").on('change', browserSync.reload);
 });
+
+gulp.task('sass', function() {
+    return gulp.src("savvy/sass/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
