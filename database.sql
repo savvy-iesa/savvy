61944d30396ec6e90bb86624f0ce3469fcd9a16b
Lucien Hubert <phillipehubert@gmail.com>
2016-11-23 00:08:24 +0100

    modification du fichier database.sql ajout de la date sur entry

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:8889
--- Généré le :  Mar 22 Novembre 2016 à 23:56
+-- Généré le :  Mer 23 Novembre 2016 à 00:06
 -- Version du serveur :  5.6.33
 -- Version de PHP :  7.0.12
 
@@ -17,17 +17,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `category`
---
-
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `entry`
 --
 
@@ -35,79 +24,13 @@ CREATE TABLE `entry` (
   `id` int(11) NOT NULL,
   `email` text NOT NULL,
   `ip` varchar(40) NOT NULL,
-  `img_key` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `entry_values`
---
-
-CREATE TABLE `entry_values` (
-  `id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `subcategory_id` int(11) NOT NULL,
-  `form_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `kpi`
---
-
-CREATE TABLE `kpi` (
-  `id` int(11) NOT NULL,
-  `name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `newsletter`
---
-
-CREATE TABLE `newsletter` (
-  `id` int(11) NOT NULL,
-  `email` text NOT NULL,
-  `ip` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `stats_kpi`
---
-
-CREATE TABLE `stats_kpi` (
-  `id` int(11) NOT NULL,
-  `id_kpi` int(11) NOT NULL,
-  `ip` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `sub_category`
---
-
-CREATE TABLE `sub_category` (
-  `id` int(11) NOT NULL,
-  `name` text NOT NULL,
-  `id_category` int(11) NOT NULL
+  `img_key` varchar(255) NOT NULL,
+  `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Index pour les tables exportées
 --
-
---
--- Index pour la table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
 
 --
 -- Index pour la table `entry`
@@ -117,76 +40,11 @@ ALTER TABLE `entry`
   ADD KEY `id` (`id`);
 
 --
--- Index pour la table `entry_values`
---
-ALTER TABLE `entry_values`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `form_id` (`form_id`);
-
---
--- Index pour la table `kpi`
---
-ALTER TABLE `kpi`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
-
---
--- Index pour la table `newsletter`
---
-ALTER TABLE `newsletter`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
-
---
--- Index pour la table `stats_kpi`
---
-ALTER TABLE `stats_kpi`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_kpi` (`id_kpi`);
-
---
--- Index pour la table `sub_category`
---
-ALTER TABLE `sub_category`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_category` (`id_category`);
-
---
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT pour la table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT pour la table `entry`
 --
 ALTER TABLE `entry`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `entry_values`
---
-ALTER TABLE `entry_values`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `kpi`
---
-ALTER TABLE `kpi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `newsletter`
---
-ALTER TABLE `newsletter`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `stats_kpi`
---
-ALTER TABLE `stats_kpi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `sub_category`
---
-ALTER TABLE `sub_category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
