acf1771699aae3c6c47408c3938e168cbfb1a644
Lucien Hubert <phillipehubert@gmail.com>
2016-11-23 23:02:09 +0100

    Création du fichier pour se connecter à la base de données

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucien Hubert
+ * Date: 23/11/2016
+ * Time: 21:17
+ */
+
+// Constantes pour la base de données
+require("config.php");
+
+try {
+    $bdd = new PDO('mysql:host='.DBHOST.';
+        port='.DBPORT.';
+        dbname='.DBNAME .';
+        charset=utf8', DBUSER, DBPASSWD);
+}
+catch(Exception $e){
+    echo 'Erreur : ' . $e->getMessage();
+    die();
+}
