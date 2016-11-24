8f81164f48079a907311a003e124721cda325e69
Lucien Hubert <phillipehubert@gmail.com>
2016-11-24 16:50:17 +0100

    Création du fichier footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucien Hubert
+ * Date: 24/11/2016
+ * Time: 16:44
+ */
+
+?>
+        <footer>
+            <p>Le content du footer</p>
+        </footer>
+    </body>
+</html>
+
+
