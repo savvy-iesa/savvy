0fd0ed938e0ace363d9d50fcd5920c573434776c
Lucien Hubert <phillipehubert@gmail.com>
2016-11-24 16:39:04 +0100

    Inclusion du head dans header.php

--- header.php
+++ header.php
@@ -5,5 +5,13 @@
  * Date: 23/11/2016
  * Time: 21:37
  */
+?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?= $pagetitle ?></title>
+        <meta charset='utf-8'/>
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
 
