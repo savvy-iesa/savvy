6a1df40bfe0fc40b92e09faa91046f4145d2783f
Lucien Hubert <phillipehubert@gmail.com>
2016-11-24 23:49:51 +0100

    Ajout de l'IA process qui est connecté à la classe de l'IA

--- controllers/FormController.php
+++ controllers/FormController.php
@@ -5,3 +5,26 @@
  * Date: 24/11/2016
  * Time: 10:47
  */
+
+require('../classes/Form.php');
+require('../classes/IAClass.php');
+
+if(isset($_POST) && !empty($POST)) {
+    $resultsForm = $_POST;
+} else {
+    header('Location: ../index.html');
+}
+
+function controlForm($resultsForm){
+  return true;
+}
+
+function ia_Process($resultsForm){
+    $IA = new IAClass($resultsForm);
+
+    $propositionsIA = $IA->IApropositions();
+    $imgIA = $IA->IAimg();
+
+
+    return true;
+}
