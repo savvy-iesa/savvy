b8705e9525ef480e0041f8d1cabece656cbbe0ea
Lucien Hubert <phillipehubert@gmail.com>
2016-11-24 16:40:13 +0100

    Ajout du fichier dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bakikr
+ * Date: 24/11/2016
+ * Time: 16:29
+ */
+
+$pagetitle = "Smartphamily - Dashboard";
+include("header.php");
+
+?>
+<div class="dashboard">
+    <p>Content Dashboard</p>
+</div>
