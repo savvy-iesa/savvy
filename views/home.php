253509195488a7656f3435c05679deae72593cea
Lucien Hubert <phillipehubert@gmail.com>
2016-11-25 09:03:46 +0100

    Création des views home et products

---
+++ views/home.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucien Hubert
+ * Date: 25/11/2016
+ * Time: 09:00
+ */
+
+$pagetitle = "Smartphamily - Home";
+include(__DIR__."/header.php");
+?>
+
+    <div>
+        <p>Home</p>
+    </div>
+
+
+<?php
+include(__DIR__."/footer.php");
+?>
---
+++ views/products.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucien Hubert
+ * Date: 25/11/2016
+ * Time: 09:01
+ */
+
+$pagetitle = "Smartphamily - les produits";
+include(__DIR__."/header.php");
+?>
+
+<div>
+    <p>Products</p>
+</div>
+
+<?php
+include(__DIR__."/footer.php");
+?>
