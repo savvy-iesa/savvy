ac7935694410ecb715c4f572b56803d1e6e26164
edouardthieffry <e.thieffry62930@gmail.com>
2016-11-25 23:26:07 +0100

    picto mail input

--- views/header.php
+++ views/header.php
@@ -12,6 +12,7 @@
     <head>
         <title><?= $pagetitle ?></title>
         <meta charset='utf-8'/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="/css/style.css" rel="stylesheet" />
     </head>
     <body>
